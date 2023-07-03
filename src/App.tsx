6ab8910443bc01d65a5041e3a648df02fea4ea2e
SokolEugene <zheksem@gmail.com>
2023-07-03 16:17:17 +0300

    First commit by MacPook. Middle of 13 samurai-way lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import {Navbar} from "./Components/Navbar";
+
 import {Footer} from "./Components/Footer";
 import {Header} from "./Components/Header";
-import {Profile} from "./Components/Profile";
+
 import {Maincontent} from "./Components/Maincontent";
 
 
