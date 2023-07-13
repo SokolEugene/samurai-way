0f553e924c44ca7d05238186206e4f8d1833ea10
SokolEugene <zheksem@gmail.com>
2023-07-13 17:31:10 +0300

    Revert "add imges"
    This reverts commit b621891695810c1202fe73f5eb306b9730b40438.

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <footer className="footer">
-            <p>--FOOTER-- it-camasutre.io -- FOOTER--</p>
+            --FOOTER-- it-camasutre.io -- FOTER--
         </footer>
     )
 }
