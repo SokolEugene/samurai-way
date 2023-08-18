0be895be4ea252063b071385f3c3e8bf2e37b649
SokolEugene <zheksem@gmail.com>
2023-08-18 22:50:02 +0300

    24 lesson of samurai way is done! by Asus

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import s from './Dialogs.module.css'
 import {NavLink} from 'react-router-dom';
+import {v1} from 'uuid';
 
 type DialogPropsType = {
     name: string
-    id: number
+    id: string
 }
 type MessagePropsType = {
     text: string
+    id: string
 }
 const Dialog = (props: DialogPropsType) => {
     return (
         <div className={s.dialog}>
-            <NavLink to={'/dialogs/'+props.id}
+            <NavLink to={'/dialogs/' + props.id}
                      className={({isActive}) => (isActive ? s.active : s.dialog)}>{props.name}</NavLink></div>
     )
 }
@@ -25,19 +27,34 @@ const Message = (props: MessagePropsType) => {
 
 
 export const Dialogs = () => {
+    let dialogsData = [
+        {id: v1().slice(0, 8), name: 'Eugene'},
+        {id: v1().slice(0, 8), name: 'Vlada'},
+        {id: v1().slice(0, 8), name: 'Lexus'},
+        {id: v1().slice(0, 8), name: 'Nick'},
+    ]
+    let messagesData = [
+        {id: v1().slice(0, 8), message: 'hi'},
+        {id: v1().slice(0, 8), message: 'hey'},
+        {id: v1().slice(0, 8), message: 'hui'},
+        {id: v1().slice(0, 8), message: 'pshe'},
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-                <Dialog name={'Eugene'} id={1}/>
-                <Dialog name={'Vlada'} id={2}/>
-                <Dialog name={'Lexus'} id={3}/>
-                <Dialog name={'Nick'} id={4}/>
+                <Dialog name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <Dialog name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <Dialog name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <Dialog name={dialogsData[3].name} id={dialogsData[3].id}/>
+
             </div>
             <div className={s.messages}>
-                <Message text={"hi"}/>
-                <Message text={"hey"}/>
-                <Message text={"hui"}/>
-                <Message text={"pshe"}/>
+                <Message text={messagesData[0].message} id={messagesData[0].id}/>
+                <Message text={messagesData[1].message} id={messagesData[1].id}/>
+                <Message text={messagesData[2].message} id={messagesData[2].id}/>
+                <Message text={messagesData[3].message} id={messagesData[3].id}/>
+
             </div>
         </div>
     )
--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,10 +1,19 @@
 
 import s from './MyPosts.module.css'
 import {Post} from './Post/Post';
+import {v1} from 'uuid';
 
+/*type PostType = {
+    id?: string
+    message: string
+    likeCounts: number
+}*/
 
-
-export const MyPosts = () => {
+export const MyPosts = (/*props: PostType*/) => {
+    let postData = [
+        {id: v1().slice(0, 8), message:"It's my first post", likeCounts:27},
+        {id: v1().slice(0, 8), message:"how are you?", likeCounts: 69},
+    ]
     return (
             <div className={s.postsBlock}>
                 <p>My Posts</p>
@@ -15,8 +24,9 @@ export const MyPosts = () => {
                     </div>
                 </div>
                 <div className={s.posts}>
-                    <Post message = {'how are you?'} likeCounts={27}/>
-                    <Post message = {"It's my first post"} likeCounts={69}/>
+                    <Post message={postData[0].message} likeCounts={postData[0].likeCounts}/*{ id={postData[0].id}}*//>
+                    <Post message={postData[1].message} likeCounts={postData[1].likeCounts}/*{ id={postData[1].id}}*//>
+
                     <Post/>
                     <Post/>
                     <Post/>
