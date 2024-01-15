b66ce34177c078ecc009e84d49a28588375b6b68
EugeneSokol <zheksem@gmail.com>
2024-01-15 15:41:01 +0200

    merge conflict resolution

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import s from './Footer.module.css'
 export const Footer = () => {
     return (
         <footer className={s.footer}>
-            <p>--FOOTER-- it-camasutre.io -- FOOTER--</p>
+            <p>--FOOTER-- it-camasutra.io -- FOOTER--</p>
         </footer>
     )
 }
